3577a7d5641acaf103d020f9b850ada9db4389e2
fausan123 <fausan.asharaf01@gmail.com>
2022-06-18 23:31:50 +0530

    updated admin panel

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import *
 # Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ("first_name", "last_name", "username", "email", "phonenumber", "date_joined", "is_approved", )
+    list_display = ("first_name", "last_name", "email", "date_joined")
+    list_filter = ("date_joined", )
+    search_fields = ("first_name__startswith", )
+
+    class Meta:
+        ordering = ("date_joined")
