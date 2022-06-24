b61e8f6acab98b2dacd7271d100133ed53cd8683
fausan123 <fausan.asharaf01@gmail.com>
2022-06-25 00:27:34 +0530

    updated exam admin interface

--- exam/admin.py
+++ exam/admin.py
@@ -6,13 +6,44 @@ from .models import *
 
 
 # Register your models here.
-admin.site.register(Exam)
-admin.site.register(Subject)
-admin.site.register(Question)
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_on", "is_open", "is_completed", "numquestions")
+    list_filter = ("created_on", "is_open", "is_completed")
+
+    def numquestions(self, obj):
+        return obj.questions.count()    
+
+    search_fields = ("name__startswith", )
+    ordering = ("-created_on", )
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "exam_link", "numquestions")
+    list_filter = ("exam", )
+
+    def numquestions(self, obj):
+        return obj.questions.count()
+
+    def exam_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:exam_exam_change", args=(obj.exam.pk,)),
+            obj.exam
+        ))
+    exam_link.short_description = "Exam"
+
+    search_fields = ("name__startswith", )
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("title", "option_a", "option_b", "option_c", "option_d", "correct_answer", "subject", "exam")
+    list_filter = ("exam", "subject", )
+
+    search_fields = ("title__startswith", )
 
 @admin.register(ExamSubmission)
 class ExamSubmissionAdmin(admin.ModelAdmin):
-    list_display = ("exam", "student_link", "submitted_on", "score")
+    list_display = ("exam", "student_link", "submitted_on", "numquestions", "questions_attempted", "score")
     list_filter = ("submitted_on", "exam")
 
     def score(self, obj):
@@ -24,6 +55,12 @@ class ExamSubmissionAdmin(admin.ModelAdmin):
         
         return score
     
+    def numquestions(self, obj):
+        return obj.exam.questions.count()
+    
+    def questions_attempted(self, obj):
+        return obj.questionanswers.count()
+    
     def student_link(self, obj):
         return mark_safe('<a href="{}">{}</a>'.format(
             reverse("admin:users_user_change", args=(obj.student.pk,)),
@@ -34,4 +71,9 @@ class ExamSubmissionAdmin(admin.ModelAdmin):
     search_fields = ("student__first_name__startswith", )
     ordering = ("-submitted_on", )
 
-admin.site.register(QuestionAnswer)
+@admin.register(QuestionAnswer)
+class QuestionAnswerAdmin(admin.ModelAdmin):
+    list_display = ("question", "exam", "answer", "correct_answer")
+
+    def correct_answer(self, obj):
+        return obj.question.correct_answer
